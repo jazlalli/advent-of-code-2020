e309dc33bbb9f95d5c2f498daec8d43e42acec49
Jaz Lalli <jaz@unmade.com>
2020-12-04 10:22:06 +0000

    Split map building from tree counting

--- day-03/index.js
+++ day-03/index.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 const path = require("path");
 
+const input = fs.readFileSync("map.txt").toString();
+const lines = input.split("\n").filter((line) => line !== "\n");
+
 // PART 1
 (function () {
   console.group("part 1");
-  const input = fs.readFileSync("map.txt").toString();
-  const lines = input.split("\n").filter((line) => line !== "\n");
-
   const treeCount = countTreesOnTheWayDown(lines, 3, 1);
   console.groupEnd();
 })();
@@ -14,8 +14,6 @@ const path = require("path");
 // PART 2
 (function () {
   console.group("part 2");
-  const input = fs.readFileSync("map.txt").toString();
-  const lines = input.split("\n").filter((line) => line !== "\n");
 
   const treeCounts = [
     countTreesOnTheWayDown(lines, 1, 1),
@@ -25,39 +23,19 @@ const path = require("path");
     countTreesOnTheWayDown(lines, 1, 2),
   ];
 
-  let logLine = "";
+  let sumLine = "";
   const finalAnswer = treeCounts.reduce((left, right, idx) => {
-    logLine += idx === 1 ? `${left} * ${right} ` : `* ${right} `;
+    sumLine += idx === 1 ? `${left} * ${right}` : ` * ${right}`;
     return left * right;
   });
 
-  console.log(`ANSWER: ${logLine}= ${finalAnswer}`);
+  console.log(`---`);
+  console.log(`ANSWER: ${sumLine} = ${finalAnswer}`);
   console.groupEnd();
 })();
 
 function countTreesOnTheWayDown(mapLines, rightStepSize, downStepSize) {
-  const rowLength = mapLines[0].length;
-  const rowCount = mapLines.length;
-
-  const downSteps = downStepSize * rowCount;
-  const rightSteps = rightStepSize * rowCount;
-  console.log(
-    `> we need to make ${downSteps} down steps and ${rightSteps} right steps`
-  );
-
-  const mapMultiplier = Math.max(downSteps, rightStepSize);
-
-  let repeatCount = Math.floor((mapMultiplier * rowCount) / rowLength);
-
-  const remainder = rowCount % rowLength;
-  if (remainder > 0) {
-    repeatCount += 1;
-  }
-
-  console.log(`> repeat map right ${repeatCount} times`);
-
-  // create the full map
-  const fullMap = mapLines.map((s) => s.repeat(repeatCount));
+  const fullMap = makeFullMap(mapLines, rightStepSize, downStepSize);
   const path = [];
 
   for (
@@ -79,6 +57,29 @@ function countTreesOnTheWayDown(mapLines, rightStepSize, downStepSize) {
   );
 
   console.log(`> encountered ${treeCount} trees`);
-  console.log(`---`);
   return treeCount;
+}
+
+function makeFullMap(inputMap, rightStepSize, downStepSize) {
+  const rowLength = inputMap[0].length;
+  const rowCount = inputMap.length;
+  const downSteps = downStepSize * rowCount;
+  const rightSteps = rightStepSize * rowCount;
+  console.log(
+    `> we need to make ${downSteps} down steps and ${rightSteps} right steps`
+  );
+
+  const mapMultiplier = Math.max(downSteps, rightStepSize);
+  const remainder = rowCount % rowLength;
+
+  let mapRepeatCount = Math.floor((mapMultiplier * rowCount) / rowLength);
+  if (remainder > 0) {
+    mapRepeatCount += 1;
+  }
+
+  console.log(`> repeat map right ${mapRepeatCount} times`);
+
+  // create the full map
+  const fullMap = inputMap.map((s) => s.repeat(mapRepeatCount));
+  return fullMap;
 }
